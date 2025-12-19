ccc4c8a8629d31a088dbbf121bfece14893907c3
Maksym Javir <Jestermaxrko@gmail.com>
2025-12-19 11:28:40 +0200

    Revert "TEst pr (#15)" (#16)
    This reverts commit c46b1b459ba40a2aa2c2c3ece99519524bb95142.

--- content.js
+++ content.js
@@ -67,7 +67,6 @@ const statusBadge = document.querySelector('[title^="Status"]');
 
 document.addEventListener('click', event => {
 
-  // test
   if (!timers.intervalTimer) {
     const buttonContent = event.target.textContent;
     const isMergeButton = buttonContent.toLowerCase().includes('merge')
